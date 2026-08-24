512e5c2781892a93ec024ecf474bc4f12e75c300
Милош <23262749+mradzic@users.noreply.github.com>
2026-08-24 11:06:19 +0000

    Configure Astro for Codespaces preview

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://mradzic.github.io',
   base: '/theoria',
+
+  server: {
+    host: true,
+    allowedHosts: [
+      '.app.github.dev',
+    ],
+  },
 });
