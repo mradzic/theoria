483ee6259cadff3d47ef389d3c3d905df8bd136a
Милош <23262749+mradzic@users.noreply.github.com>
2026-08-24 08:00:46 +0000

    Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://mradzic.github.io',
+  base: '/theoria',
+});
